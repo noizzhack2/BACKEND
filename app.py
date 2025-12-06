32a2ca20e269a8d66b51a3c48db35c0057c7eee8
Noizz5 <noizzhack2@gmail.com>
2025-12-07 00:18:20 +0200

    run action

--- app.py
+++ app.py
@@ -4,4 +4,5 @@ from main import API
 if __name__ == "__main__":
     import uvicorn
     port = int(os.environ.get("PORT", "8000"))
+    print("Starting Uvicorn server...")
     uvicorn.run("main:API", host="0.0.0.0", port=port, reload=False)
